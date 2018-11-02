9222aa8ab98060271334c324de6183e9f7450123
darshan3011 <44117471+darshan3011@users.noreply.github.com>
2018-11-02 20:54:02 +0530

    Update DriversRun.java

--- DriversRun.java
+++ DriversRun.java
@@ -1,15 +1,19 @@
 public class DriversRun{
 
 	public static void main(String[] args) {
-	String browser="Firefox";
+	String browser="Chrome";
 	String url="http://toolsqa.com/automation-practice-form/";
 	initDriver ini= new initDriver();
 	ini.run(browser,url);
-	
+	ini.clickbtn();
+	/*ini.displaytxt();
 	ini.typeFirstname("abcd");
 	ini.typeLastname("def");
 	ini.selectExp();
 	ini.selectprofession();
+	ini.selectcountry();
+	ini.mutliplevalues();
+	ini.clickLink();*/
 	}
 
 }
